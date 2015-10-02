5ef4e0234250818952b7a52e0e94fd39f708b5cd
Gabriel Vieira <gabrielvie@gmail.com>
2015-10-02 00:30:08 -0300

    Realocado arquivo de teste, parar rodar os testes usar o comando "npm test".

---
+++ test.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var should 	= require('should'),
+	expect 	= require('expect.js'),
+	request = require('supertest');
+
+require('./test/user.spec');
+require('./test/creditcards.spec');
