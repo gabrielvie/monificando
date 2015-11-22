06cf5ce56dbe3f5e78e1c850ce30e63cf2d17ffb
Gabriel Santos <gabrielvie@gmail.com>
2015-11-22 17:42:51 -0200

    Beta 1.3

--- config/env/production.env.js
+++ config/env/production.env.js
@@ -6,7 +6,7 @@ module.exports = {
 	app: {
 		title: 'Monificando - Development Edition'
 	},
-	port: 3000,
+	port: 8080,
 	database: function() {
 		return 'mongodb://monificando:adfa031c828872c72c3c6c58f484dd52@127.0.0.1:27017/monificando'
 	},
